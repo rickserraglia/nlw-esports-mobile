5e7119d2232b602671c1aed8cd27206ca81da279
Henrique Serraglia <rickserraglia@gmail.com>
2022-09-13 19:53:20 -0300

    intro page of mobile app - day 2

--- App.tsx
+++ App.tsx
@@ -1,37 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import {
+	Inter_400Regular,
+	Inter_600SemiBold,
+	Inter_700Bold,
+	Inter_900Black,
+	useFonts
+} from '@expo-google-fonts/inter';
+import { StatusBar } from 'react-native';
 
-interface ButtonProps {
-	text: string;
-}
-
-function Button({ text }: ButtonProps) {
-	return (
-		<TouchableOpacity>
-			<Text>{text}</Text>
-		</TouchableOpacity>
-	);
-}
+import { Background } from './src/components/Background';
+import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
+	const [fontsLoaded] = useFonts({
+		Inter_400Regular,
+		Inter_600SemiBold,
+		Inter_700Bold,
+		Inter_900Black
+	});
+
 	return (
-		<View style={styles.container}>
-			<Text>Hello React Native!</Text>
-
-			<Button text="Texto Botão 1" />
-			<Button text="Texto Botão 2" />
-			<Button text="Texto Botão 3" />
-			<Button text="Hello World Button" />
-
-			<StatusBar style="auto" />
-		</View>
+		<Background>
+			<StatusBar
+				barStyle="light-content"
+				backgroundColor="transparent"
+				translucent
+			/>
+			{fontsLoaded ? <Home /> : <Loading />}
+		</Background>
 	);
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: 'center',
-		justifyContent: 'center'
-	}
-});
