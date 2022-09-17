c5efd321f3624eb0c563b19023a8ebbe90b0c9a8
Henrique Serraglia <rickserraglia@gmail.com>
2022-09-17 15:59:47 -0300

    added styles, routing and api integration

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -1,14 +1,21 @@
 import { LinearGradient } from 'expo-linear-gradient';
-import { ImageBackground, ImageSourcePropType, Text, TouchableOpacity, TouchableOpacityProps } from 'react-native';
+import {
+	ImageBackground,
+	Text,
+	TouchableOpacity,
+	TouchableOpacityProps
+} from 'react-native';
 
 import { THEME } from '../../theme';
 import { styles } from './styles';
 
 export interface GameCardProps {
 	id: string;
-	name: string;
-	ads: string;
-	cover: ImageSourcePropType;
+	title: string;
+	_count: {
+		ads: number;
+	};
+	bannerUrl: string;
 }
 
 interface GameCardInternalProps extends TouchableOpacityProps {
@@ -18,10 +25,10 @@ interface GameCardInternalProps extends TouchableOpacityProps {
 export function GameCard({ data, ...rest }: GameCardInternalProps) {
 	return (
 		<TouchableOpacity style={styles.container} {...rest}>
-			<ImageBackground style={styles.cover} source={data.cover}>
+			<ImageBackground style={styles.cover} source={{ uri: data.bannerUrl }}>
 				<LinearGradient colors={THEME.COLORS.FOOTER} style={styles.footer}>
-					<Text style={styles.name}>{data.name}</Text>
-					<Text style={styles.ads}>{data.ads} anúncios</Text>
+					<Text style={styles.name}>{data.title}</Text>
+					<Text style={styles.ads}>{data._count.ads} anúncios</Text>
 				</LinearGradient>
 			</ImageBackground>
 		</TouchableOpacity>
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,27 +1,48 @@
-import { FlatList, Image, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { useEffect, useState } from 'react';
+import { FlatList, Image } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
-import { GameCard } from '../../components/GameCard';
+import { Background } from '../../components/Background';
+import { GameCard, GameCardProps } from '../../components/GameCard';
 import { Heading } from '../../components/Heading';
-import { GAMES } from '../../utils/games';
 import { styles } from './styles';
 
 export function Home() {
+	const [games, setGames] = useState<GameCardProps[]>();
+
+	useEffect(() => {
+		fetch('http://127.0.0.1:3333/games')
+			.then((response) => response.json())
+			.then((data) => setGames(data));
+	}, []);
+
+	const navigation = useNavigation();
+
+	function handleOpenGame({ id, title, bannerUrl }: GameCardProps) {
+		navigation.navigate('game', { id, title, bannerUrl });
+	}
+
 	return (
-		<View style={styles.container}>
-			<Image source={logoImg} style={styles.logo} />
-			<Heading
-				title="Encontre seu duo!"
-				subtitle="Selecione o game que deseja jogar..."
-			/>
-			<FlatList
-				data={GAMES}
-				keyExtractor={(item) => item.id}
-				horizontal
-				showsHorizontalScrollIndicator={false}
-				contentContainerStyle={styles.contentList}
-				renderItem={({ item }) => <GameCard data={item} />}
-			/>
-		</View>
+		<Background>
+			<SafeAreaView style={styles.container}>
+				<Image source={logoImg} style={styles.logo} />
+				<Heading
+					title="Encontre seu duo!"
+					subtitle="Selecione o game que deseja jogar..."
+				/>
+				<FlatList
+					data={games}
+					keyExtractor={(item) => item.id}
+					horizontal
+					showsHorizontalScrollIndicator={false}
+					contentContainerStyle={styles.contentList}
+					renderItem={({ item }) => (
+						<GameCard data={item} onPress={() => handleOpenGame(item)} />
+					)}
+				/>
+			</SafeAreaView>
+		</Background>
 	);
 }
